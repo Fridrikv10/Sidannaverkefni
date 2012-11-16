8d16ebdc651940a2c6dd096abd33e39fa853863a
Fridrik <fridrikv10@ru.is>
2012-11-16 15:21:12 +0000

    Added a class 'TicTacToe'

---
+++ Verkefni/src/StartingTests/TicTacToe.java
@@ -0,0 +1,13 @@
+package StartingTests;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: LENOVO
+ * Date: 16.11.2012
+ * Time: 15:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class TicTacToe
+{
+    
+}
